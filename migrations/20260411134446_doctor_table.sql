5db5a1750a82bc704fc7cbe199b2206e310becb7
Phantomvv1 <krisi_andreev@abv.bg>
2026-04-11 17:28:22 +0300

    Fixed mistakes in the migrations and now they all run as they should

--- migrations/20260411134446_doctor_table.sql
+++ migrations/20260411134446_doctor_table.sql
@@ -4,7 +4,7 @@ create table if not exists doctors(
 	name text not null,
 	email text unique not null,
 	address text not null,
-	working_hours_id int REFERENCES working_hours(id) no delete cascade
+	working_hours_id int REFERENCES working_hours(id) on delete cascade
 );
 
 -- +goose Down
--- migrations/20260411140946_visits_table.sql
+++ migrations/20260411140946_visits_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE visits (
 	start_time timestamp not null,
 	visit_time interval not null,
 	patient_id int references patients(id),
-	doctor_id int references doctors(id),
+	doctor_id int references doctors(id)
 );
 
 -- +goose Down
